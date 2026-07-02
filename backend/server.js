16fe910197730946a1ebfc959dd2b288120d9ff9
manhcuong-contact <manhcuong.contact@gmail.com>
2026-07-02 21:00:59 +0700

    Fix Express 5 wildcard routing error

--- backend/server.js
+++ backend/server.js
@@ -80,9 +80,10 @@ if (process.env.NODE_ENV === 'production') {
   
   app.use(express.static(frontendPath, { extensions: ['html'] }));
   
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
   });
+
 }
 
 httpServer.listen(port, () => {
